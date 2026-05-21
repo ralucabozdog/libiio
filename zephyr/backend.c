bd37e0ca354d29f967a84bd022e42204434951d5
Vladislav Pejic <vladislav.pejic@orioninc.com>
2026-05-21 12:30:00 +0200

    zephyr: Add set/get trigger and shutdown
    Added set_trigger and get_trigger API to the zephyr backend.
    Added shutdown API to the zephyr backend to free allocated trigger data.
    
    Signed-off-by: Vladislav Pejic <vladislav.pejic@analog.com>

--- zephyr/backend.c
+++ zephyr/backend.c
@@ -60,7 +60,26 @@ zephyr_write_attr(const struct iio_attr *attr, const char *src, size_t len)
 static const struct iio_device *
 zephyr_get_trigger(const struct iio_device *dev)
 {
-	return dev;
+	const struct iio_device * trigger;
+
+	if (iio_device_is_trigger(dev)) {
+		return iio_ptr(-ENOENT);
+	} else {
+		trigger = (const struct iio_device *) iio_device_get_data(dev);
+		
+		if (!trigger) {
+			return iio_ptr(-ENODEV);
+		} else {
+			return trigger;
+		}
+	}
+}
+
+int zephyr_set_trigger(const struct iio_device *dev,
+			const struct iio_device *trigger)
+{
+	iio_device_set_data((struct iio_device *)dev, (void *)trigger);
+	return 0;
 }
 
 static struct iio_context *
@@ -149,7 +168,7 @@ static void zephyr_close_buffer(struct iio_buffer_pdata *pdata)
 }
 
 static int zephyr_enable_buffer(struct iio_buffer_pdata *pdata,
-			size_t nb_samples, bool enable, bool cyclic)
+				size_t nb_samples, bool enable, bool cyclic)
 {
 	pdata->enabled = enable;
 	return 0;
@@ -162,9 +181,9 @@ static void zephyr_cancel_buffer(struct iio_buffer_pdata *pdata)
 
 
 static int iio_device_read_channel_raw(const struct device *zephyr_dev,
-											const struct iio_channel *chn,
-											const struct iio_device *iio_dev,
-											char *buf, size_t len)
+									   const struct iio_channel *chn,
+									   const struct iio_device *iio_dev,
+									   char *buf, size_t len)
 {
 	const struct iio_attr *raw_attr = NULL;
 	unsigned int nb_attrs = iio_channel_get_attrs_count(chn);
@@ -184,7 +203,7 @@ static int iio_device_read_channel_raw(const struct device *zephyr_dev,
 }
 
 static ssize_t zephyr_readbuf(struct iio_buffer_pdata *pdata,
-					void *dst, size_t len)
+				  void *dst, size_t len)
 {
 	const struct device *zephyr_dev = pdata->zephyr_dev;
 	const struct iio_device *iio_dev = pdata->iio_dev;
@@ -192,51 +211,51 @@ static ssize_t zephyr_readbuf(struct iio_buffer_pdata *pdata,
 	unsigned int nb_channels = iio_device_get_channels_count(iio_dev);
 	char raw_buf[32];
 	int ret;
-
+	
 	unsigned int enabled_channels = 0;
 	for (unsigned int ch_idx = 0; ch_idx < nb_channels; ch_idx++) {
 		const struct iio_channel *chn = iio_device_get_channel(iio_dev, ch_idx);
-		if (iio_channel_is_scan_element(chn) &&
+		if (iio_channel_is_scan_element(chn) && 
 			iio_channel_is_enabled(chn, pdata->mask)) {
 			enabled_channels++;
 		}
 	}
-
+	
 	if (enabled_channels == 0)
 		return -EINVAL;
-
+	
 	const struct iio_channel *first_chn = NULL;
 	for (unsigned int ch_idx = 0; ch_idx < nb_channels; ch_idx++) {
 		const struct iio_channel *chn = iio_device_get_channel(iio_dev, ch_idx);
-		if (iio_channel_is_scan_element(chn) &&
+		if (iio_channel_is_scan_element(chn) && 
 			iio_channel_is_enabled(chn, pdata->mask)) {
 			first_chn = chn;
 			break;
 		}
 	}
-
+	
 	if (!first_chn)
 		return -EINVAL;
-
+	
 	const struct iio_data_format *fmt = iio_channel_get_data_format(first_chn);
 
 	size_t bytes_per_channel = DIV_ROUND_UP(fmt->length, BITS_PER_BYTE);
 	size_t bytes_per_sample = enabled_channels * bytes_per_channel;
 	size_t num_samples = len / bytes_per_sample;
 	size_t samples_written = 0;
-
+	
 	for (size_t sample_idx = 0; sample_idx < num_samples; sample_idx++) {
 		for (unsigned int ch_idx = 0; ch_idx < nb_channels; ch_idx++) {
 			const struct iio_channel *chn = iio_device_get_channel(iio_dev, ch_idx);
-
+			
 			if (!iio_channel_is_scan_element(chn))
 				continue;
 			if (!iio_channel_is_enabled(chn, pdata->mask))
 				continue;
-
-			ret = iio_device_read_channel_raw(zephyr_dev, chn, iio_dev,
-										raw_buf, sizeof(raw_buf));
-
+			
+			ret = iio_device_read_channel_raw(zephyr_dev, chn, iio_dev, 
+											 raw_buf, sizeof(raw_buf));
+			
 			int32_t raw_value;
 			if (ret < 0) {
 				raw_value = 0;
@@ -247,9 +266,9 @@ static ssize_t zephyr_readbuf(struct iio_buffer_pdata *pdata,
 					raw_value = (int32_t)strtoul(raw_buf, NULL, 10);
 				}
 			}
-
+			
 			raw_value <<= fmt->shift;
-
+			
 			if (fmt->is_be) {
 				for (int i = bytes_per_channel - 1; i >= 0; i--) {
 					*out++ = (uint8_t)((raw_value >> (i * 8)) & 0xFF);
@@ -262,39 +281,39 @@ static ssize_t zephyr_readbuf(struct iio_buffer_pdata *pdata,
 		}
 		samples_written++;
 	}
-
+	
 	return (ssize_t)(samples_written * bytes_per_sample);
 }
 
 static struct iio_block_pdata *
 zephyr_create_block(struct iio_buffer_pdata *pdata, size_t size, void **data)
 {
-    struct iio_block_pdata *block;
+	struct iio_block_pdata *block;
 
-    block = zalloc(sizeof(*block));
+	block = zalloc(sizeof(*block));
     if (!block)
-        return iio_ptr(-ENOMEM);
+		return iio_ptr(-ENOMEM);
 
-    block->data = malloc(size);
-    if (!block->data) {
-        free(block);
-        return iio_ptr(-ENOMEM);
-    }
+	block->data = malloc(size);
+	if (!block->data) {
+		free(block);
+		return iio_ptr(-ENOMEM);
+	}
 
-    block->size = size;
+	block->size = size;
     block->buf_pdata = pdata;
-    *data = block->data;
+	*data = block->data;
 
-    return block;
+	return block;
 }
 
 static void zephyr_free_block(struct iio_block_pdata *block)
 {
-    if (block) {
+	if (block) {
         if (block->data)
-            free(block->data);
-        free(block);
-    }
+			free(block->data);
+		free(block);
+	}
 }
 
 static int zephyr_enqueue_block(struct iio_block_pdata *block,
@@ -302,24 +321,33 @@ static int zephyr_enqueue_block(struct iio_block_pdata *block,
 {
     (void)bytes_used;
     (void)cyclic;
-    return 0;
+	return 0;
 }
 
 static int zephyr_dequeue_block(struct iio_block_pdata *block, bool nonblock)
 {
-    struct iio_buffer_pdata *buf_pdata = block->buf_pdata;
+	struct iio_buffer_pdata *buf_pdata = block->buf_pdata;
     ssize_t ret;
 
     (void)nonblock;
 
     if (!buf_pdata->enabled)
-        return -EBADF;
+		return -EBADF;
 
     ret = zephyr_readbuf(buf_pdata, block->data, block->size);
     if (ret < 0)
         return (int)ret;
+	
+	return 0;
+}
 
-    return 0;
+static void zephyr_shutdown(struct iio_context *ctx)
+{
+	for (int i = 0; i < ctx->nb_devices; i++) {
+		if (iio_device_is_trigger(ctx->devices[i])) {
+			free(iio_device_get_pdata(ctx->devices[i]));
+		}
+	}
 }
 
 static const struct iio_backend_ops zephyr_ops = {
@@ -327,17 +355,18 @@ static const struct iio_backend_ops zephyr_ops = {
 	.read_attr = zephyr_read_attr,
 	.write_attr = zephyr_write_attr,
 	.get_trigger = zephyr_get_trigger,
-    //.set_trigger = zephyr_set_trigger,
-    .open_buffer = zephyr_open_buffer,
-    .close_buffer = zephyr_close_buffer,
-    .enable_buffer = zephyr_enable_buffer,
-    .cancel_buffer = zephyr_cancel_buffer,
-    .readbuf = zephyr_readbuf,
-    //.writebuf = zephyr_writebuf,
-    .create_block = zephyr_create_block,
-    .free_block = zephyr_free_block,
-    .enqueue_block = zephyr_enqueue_block,
-    .dequeue_block = zephyr_dequeue_block,
+	.set_trigger = zephyr_set_trigger,
+	.open_buffer = zephyr_open_buffer,
+	.close_buffer = zephyr_close_buffer,
+	.enable_buffer = zephyr_enable_buffer,
+	.cancel_buffer = zephyr_cancel_buffer,
+	.readbuf = zephyr_readbuf,
+	//.writebuf = zephyr_writebuf,
+	.create_block = zephyr_create_block,
+	.free_block = zephyr_free_block,
+	.enqueue_block = zephyr_enqueue_block,
+	.dequeue_block = zephyr_dequeue_block,
+	.shutdown = zephyr_shutdown,
 };
 
 const struct iio_backend iio_external_backend = {
