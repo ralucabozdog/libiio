c799a94a48c2791ebb380ab5ff97e708f2cf960b
Raluca Bozdog <raluca.bozdog@analog.com>
2026-05-22 08:48:30 +0300

    zephyr: Use APARD32690 on-board ADC
    Changes for iiod sample to work with the ADC on the APARD32690
    board instead of the emulated ADC.
    
    Signed-off-by: Raluca Bozdog <raluca.bozdog@analog.com>

--- zephyr/drivers/iio_device/adc.c
+++ zephyr/drivers/iio_device/adc.c
@@ -624,7 +624,8 @@ static const struct iio_device_adc_config iio_device_adc_config_##inst = {		\
 	.buffer_name = DT_INST_PROP_OR(inst, buffer_name, "buffer"),			\
 };											\
 											\
-IIO_DEVICE_DT_INST_DEFINE(inst, iio_device_adc_init, NULL,				\
+IIO_DEVICE_DT_INST_DEFINE(inst, DT_INST_PROP_OR(inst, io_name, NULL),			\
+	iio_device_adc_init, NULL,							\
 	&iio_device_adc_data_##inst, &iio_device_adc_config_##inst,			\
 	POST_KERNEL, CONFIG_LIBIIO_IIO_DEVICE_ADC_INIT_PRIORITY,			\
 	&iio_device_adc_driver_api);
